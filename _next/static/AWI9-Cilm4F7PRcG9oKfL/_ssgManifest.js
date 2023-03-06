62627d0ebf6fa722fcaf25e2f7defa59d5d88115
8vips CI <8vips@users.noreply.github.com>
2023-03-06 19:54:46 +0000

    Updates

---
+++ _next/static/AWI9-Cilm4F7PRcG9oKfL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fartists","\u002Feditions","\u002Ffaq","\u002F","\u002Fjoin","\u002Fartists\u002F[artist]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
