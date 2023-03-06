0efbaab60c271b8b0090ca11a6edefef454857d9
Drewler <drewler@gmail.com>
2023-03-06 20:04:50 +0100

    Updates

---
+++ _next/static/G3SIW-6iNlXnfb4-YUmFM/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ffaq","\u002Fartists","\u002Fjoin","\u002Fabout","\u002Feditions","\u002F","\u002Fartists\u002F[artist]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/Q_p__bXn-jQhyUbTEjhs9/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Feditions","\u002Fartists","\u002Fartists\u002F[artist]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
